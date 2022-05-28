7e2ae53d1e181c4720f0e6869c912126487fcf6b
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-05-27 22:12:23 -0500

    more icons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import HomePage from "./Pages/HomePage";
 import "bootstrap"
 import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 
 function App() {
   return (
     <>
+        <Navbar/>
         <HomePage/>
+        <Footer/>
     </>
   );
 }
