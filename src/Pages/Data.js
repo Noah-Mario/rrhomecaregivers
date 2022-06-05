bf7ad080a598758c3d33bb847237bf681d8e94f4
Mario Granados <mario.alberto.granados1@gmail.com>
2022-06-04 23:57:53 -0500

    v2

---
+++ src/Pages/Data.js
@@ -0,0 +1,52 @@
+export const mission = {
+    title: 'Our Mission',
+    content: 'Our mission is to provide compassionate and skillful home care services of superior quality to elderly and disabled persons in their performance of activities of daily living in their own homes.'
+}
+
+export const goal = {
+    title: 'Our Goal',
+    content: 'Our Goal is to provide superior home care services allowing our clients to maintain their dignity and quality of life, increase their ability to lead an independent life, and integrate them into the community as much as possible.'
+}
+
+export const payment = {
+    title: 'Payment Methods',
+    content: 'R & R Home Caregivers accepts several forms of payments. We accepts Long-Term Care Insurance, VA Aid & Attendance, Private Insurance, private payment and Medicaid.'
+}
+
+export const whyUs = {
+    title: 'Why R & R Home Caregivers?',
+    leftCol: 'R & R Home Caregivers is a woman owned home care agency providing personal assistance care to the elderly of San Antonio (Bexar County). Our goal is to maintain the size of the company at a level where the owner will always be the client’s primary point of contact.\n' +
+        'R & R is licensed by the state of Texas, insured, and takes tremendous pride in hiring only loving and caring providers. Our providers are generally, mature, experienced and well trained.\n' +
+        'We often meet the family of our clients at a time when their loved ones needs exceeds the family’s ability to provide the required care. This is a difficult and stressful experience for the family.',
+    rightCol: 'Our goal is to quickly and efficiently provide the care their loved one requires. We will proactively work with the client and their family to provide the required care. Additionally, we will assist in the management of the client’s other care providers. This might include an independent living facility, an assisted living facility, memory care facility or in the comfort of their own home. Our goal is to quickly reduce the stress on the family and provide the family confidence that their loved one is in good hands.\n' +
+        'Our motto is “where clients become family.” We take this motto seriously. We are being invited into the client’s home. Our caregivers often spend more one on one time with the client than anyone else. We care for the family’s loved one as if they were our own family. We don’t know any other way.',
+    motto: 'THE COMMITMENT WE MAKE TO YOU TODAY IS THE SAME COMMITMENT WE WILL MAKE TOMORROW, NEXT MONTH, NEXT YEAR.'
+}
+
+export const services = {
+    title: 'services',
+    content: 'Assistance with Personal: Care Dressing, Transferring, Grooming, Bathing, etc.\n' +
+        'Home Management: Light Housekeeping, Meal Planning, Assistance with Self-Administered Medications, etc.\n' +
+        'Free Home Care Assessment\n' +
+        'Individualized Plan of Care\n' +
+        'Quality Control Visits'
+}
+
+export const personal = {
+    title: 'Assistance with Personal',
+    content: 'Care Dressing, Transferring, Grooming, Bathing, etc.'
+}
+
+export const homeManagement = {
+    title: 'Home Management',
+    content: 'Light Housekeeping, Meal Planning, Assistance with Self-Administered Medications, etc.',
+}
+export const homeAssessment = {
+    title: 'Free Home Care Assessment'
+}
+export const carePlan = {
+    title: 'Individualized Plan of Care',
+}
+export const QA = {
+    title: ''
+}
