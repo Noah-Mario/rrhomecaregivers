381f473e8617a985d0116efe35a772ebac80c0b4
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-06-07 21:02:18 -0500

    tried fetch request instead of axios still no work

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import Axios from 'axios'
+// import Axios from 'axios'
 
 
 
@@ -9,16 +9,42 @@ function Testimonials(){
     const [review, setReview] = useState('');
     const [imageURL, setImageURL] = useState('');
 
+    // Example POST method implementation:
+    async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                // 'Content-Type': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
     const submitReview = () =>{
         console.log("here")
-        Axios.post('localhost:3306/api/insert', {titleName:titleName, review:review, imageURL:imageURL})
-            .then((data)=>{
-                console.log("now here")
-                console.log(data);
-        }).catch((err)=>{
-            console.log(err)
-        });
-    };
+
+        postData('http://localhost:3001/api/insert', {titleName:titleName, review:review, imageURL:imageURL})
+            .then(response => {
+                if (!response.ok){
+                    throw new Error('Network response not okay')
+                }
+            })
+            .then(data => {
+                console.log(data); // JSON data parsed by `data.json()` call
+            }).catch(error => {
+            console.error('there is a problem with your fetch operation: ', error)
+        })
+
+
+    }
 
     return(<>
 
