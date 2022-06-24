e3237b4a8732d0a51da2ba12229288e06562d6f7
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-06-24 13:48:49 -0500

    new image model with many relationship

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -1,32 +1,48 @@
 import {useState, useEffect} from "react";
 import axios from 'axios';
-import {Formik, Form, Field, ErrorMessage} from 'formik';
+
 
 
 
 
 function Testimonials() {
 
-
-    const initialValues ={
-        title: "",
-        review: "",
-        imageUrl: "",
-        imageUrl2: "",
-    }
-
+    const [title, setTitle] = useState ("")
+    const [review, setReview] = useState("")
+    const [url, setUrl] = useState("")
     const [listOfPosts, setListOfPosts] = useState([]);
+    const [listOfImages, setListOfImages] = useState([]);
 
-    const onSubmit = (data) => {
-        axios.post("http://localhost:3306/posts", data).then((e) =>
-            setListOfPosts(e.data)
-        )
-        window.location.reload();
+
+    let endpoints = [
+        "http://localhost:3306/posts",
+        "http://localhost:3306/images"
+    ];
+
+    const onSubmit = () => {
+        let i = 0;
+        while ( i < endpoints.length) {
+            if (endpoints[i] === "http://localhost:3306/images") {
+                axios.post("http://localhost:3306/images", {url: url}).then((res) => {
+                    console.log(res)
+                })
+            }
+                axios.post("http://localhost:3306/posts",{title: title, review: review} ).then((res) => {
+                    console.log(res)
+                })
+            i++
+        }
     }
+
 
     useEffect(() => {
         axios.get("http://localhost:3306/posts").then((res) =>
             setListOfPosts(res.data))
+    }, [])
+
+    useEffect(() => {
+        axios.get("http://localhost:3306/images").then((res) =>
+            setListOfImages(res.data))
     }, [])
 
 
@@ -34,7 +50,7 @@ function Testimonials() {
         axios.delete(`http://localhost:3306/posts/${id}`).then((res) =>
             console.log("sent")
         )
-        window.location.reload()
+        // window.location.reload()
     }
 
     const getUser = (e) => {
@@ -57,39 +73,39 @@ function Testimonials() {
             Testimonials
         </h1>
 
-        <div className="w-100 newCard row d-flex justify-content-around">
+        <div className="row d-flex justify-content-around">
             {listOfPosts.map((value, key) => {
-                return <div
-                    className="container-fluid rCorners2 card-shadow card-w box2 row mb-5 d-flex justify-content-center">
+                return<>
+                   <div className=" rCorners2 card-shadow card-w box2 row  mb-5 ">
                     <h1 className="text-center titleText">{value.title}</h1>
                     <p className="text-center card-t-purp">{value.review}</p>
-                    <img src={value.imageUrl} className="image col-4"/>
+                       {listOfImages.map((val, key) => {
+                           return<>
+                               <img alt="uploaded" src={val.url} className="image col-4"/>
+                           </>
+                       })}
                     <p className="hide">{value.id}</p>
                     <button onClick={() => {deleteReview(value.id)}} className="btn-red createPost hide" role="button" type="submit">Delete</button>
                 </div>
+                </>
+
             })}
+
         </div>
 
         <div className="createPost hide">
-            <Formik initialValues={initialValues} onSubmit={onSubmit} >
-                <Form>
+                <form onSubmit={onSubmit}>
                     <div className="mb-2">
-                        <Field className="form" id="inoutCreatePost"  name="title" placeholder="Author" />
+                        <input className="form" id="inoutCreatePost"  name="title" placeholder="Author" onChange={(e => setTitle(e.target.value) )} />
                     </div>
                     <div className="mb-2">
-                        <Field id="inoutCreatePost" className="review" as="textarea" name="review" placeholder="Review" />
+                        <input id="inoutCreatePost" className="review" as="textarea" name="review" placeholder="Review" onChange={(e => setReview(e.target.value))} />
                     </div>
                     <div className="mb-2">
-                        <label>ImageUrl: </label>
-                        <Field id="inoutCreatePost"  name="imageUrl" placeholder="ImageUrl" />
-                    </div>
-                    <div className="mb-2">
-                        <label>ImageUrl2: </label>
-                        <Field id="inoutCreatePost"  name="imageUrl2" placeholder="ImageUrl2" />
+                        <input id="inoutCreatePost"  name="imageUrl" placeholder="ImageUrl" onChange={(e => setUrl(e.target.value))}/>
                     </div>
                     <button className="button-36" role="button" type="submit">Submit</button>
-                </Form>
-            </Formik>
+                </form>
         </div>
         <button onClick={getUser} className="btn-red" role="button" type="submit">Write Post</button>
 
